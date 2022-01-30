1d9bef5eb05d114c4037dcb8508d7817f4652119
austin espinal <austinespi@gmail.com>
2022-01-30 03:31:25 -0500

    need to have image_name include file type. will continue to work

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,38 +1,38 @@
 import { getStorage, ref, deleteObject } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-storage.js";
 
-function getValue(id) {
-    return document.getElementById(id).value;
-}
-
+// function getValue(id) {
+//     return document.getElementById(id);
+// }
+//needs to be mountain.jpg in order to delete
 async function deleteFormHandler(event) {
     event.preventDefault();
 
-    const imageName = getValue("image-name");
+    const imageText = document.getElementById("image-name");
+    const imageName = imageText.innerText;
 
     console.log(imageName);
 
-    // const storage = getStorage();
-    // // const imageRef =ref(storage, 'image/' + );
+    const storage = getStorage();
+    const imageRef =ref(storage, 'image/' + imageName);
 
-    // deleteObject(imageRef).then(()=> {
-        
-    // })
-    // .catch((err) => {
-    //     console.log(err);
-    // })
+    deleteObject(imageRef).then(()=> {
+        const id = window.location.toString().split('/')[
+            window.location.toString().split('/').length - 1
+        ];
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'DELETE'
+        });
+    
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
-    // const id = window.location.toString().split('/')[
-    //     window.location.toString().split('/').length - 1
-    // ];
-    // const response = await fetch(`/api/posts/${id}`, {
-    //     method: 'DELETE'
-    // });
-
-    // if (response.ok) {
-    //     document.location.replace('/profile');
-    // } else {
-    //     alert(response.statusText);
-    // }
     
 }
 
