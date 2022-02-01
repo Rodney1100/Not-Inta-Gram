4b78073dbe4ab64c73832b3442eff6c79cff1967
Rodney P <rodney.plancher@gmail.com>
2022-02-01 13:56:42 -0500

    adding some css

--- server.js
+++ server.js
@@ -1,49 +1,52 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const path = require('path');
-const helpers = require('./utils/helpers');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({helpers});
-const fileupload = require('express-fileupload');
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+const helpers = require("./utils/helpers");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const fileupload = require("express-fileupload");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const session = require('express-session');
+const session = require("express-session");
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(session(sess));
 
 app.use(express.json());
-app.use(fileupload({useTempFiles: true}))
+app.use(fileupload({ useTempFiles: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
