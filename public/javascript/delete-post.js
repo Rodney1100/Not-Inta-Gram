d9187734f2b41c4bc956727c3282866265ff81d7
austin espinal <austinespi@gmail.com>
2022-02-04 00:06:35 -0500

    repaired delete function

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,70 +1,51 @@
-//code needed to delete image from cloud as well. will implement at a later date
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
-import {
-  getStorage,
-  ref,
-  deleteObject,
-} from "https://www.gstatic.com/firebasejs/9.6.5/firebase-storage.js";
+import { getStorage, ref, deleteObject } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDM8VSVoUfg4wwXvYr70hiKyBVkYHTgp8",
-  authDomain: "not-inta-gram.firebaseapp.com",
-  projectId: "not-inta-gram",
-  storageBucket: "not-inta-gram.appspot.com",
-  messagingSenderId: "567172899580",
-  appId: "1:567172899580:web:b9c3966dee8ba94347ed79",
+    apiKey: "AIzaSyDDM8VSVoUfg4wwXvYr70hiKyBVkYHTgp8",
+    authDomain: "not-inta-gram.firebaseapp.com",
+    projectId: "not-inta-gram",
+    storageBucket: "not-inta-gram.appspot.com",
+    messagingSenderId: "567172899580",
+    appId: "1:567172899580:web:b9c3966dee8ba94347ed79"
 };
 const firebaseApp = initializeApp(firebaseConfig);
 
 const storage = getStorage(firebaseApp);
 
 async function deleteFormHandler(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const storage = getStorage();
-  // const imageRef =ref(storage, 'image/' + );
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
 
-  deleteObject(imageRef)
-    .then(() => {})
-    .catch((err) => {
-      console.log(err);
-    });
+    fetch(`/api/posts/${id}`)
+        .then(async res => {
+            const postData = await res.json()
+            console.log(postData);
+            const imageRef = ref(storage, postData.image_url)
+            deleteObject(imageRef).then(() => {
+                console.log('successfully deleted image file off cloud');
+            })
+                .catch((err) => {
+                    console.log(err);
+                    alert(err.message);
+                })
+            fetch(`/api/posts/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(() => {
+                        console.log('successfully deleted post');
+                        document.location.replace('/profile');
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        alert(err.message);
+                    });
 
-  const imageText = document.getElementById("image-name");
-  const imageName = imageText.innerText;
 
-  console.log(imageName);
-
-  const id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
-
-  fetch(`/api/posts/${id}`).then(async (res) => {
-    const postData = await res.json();
-    console.log(postData);
-    const imageRef = ref(storage, postData.image_url);
-    deleteObject(imageRef)
-      .then(() => {
-        console.log("successfully deleted image file off cloud");
-        fetch(`/api/posts/${id}`, {
-          method: "DELETE",
         })
-          .then(() => {
-            console.log("successfully deleted post");
-            document.location.replace("/profile");
-          })
-          .catch((err) => {
-            console.log(err);
-            alert(err.message);
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-        alert(err.message);
-      });
-  });
 }
 
-document
-  .querySelector(".delete-post-btn")
-  .addEventListener("click", deleteFormHandler);
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
