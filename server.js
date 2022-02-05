ac9a287b11dad5d294f38028ffa6676d724bad63
austin espinal <austinespi@gmail.com>
2022-02-05 01:09:25 -0500

    add secret env and adjusted login style

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: "Super secret secret",
+  secret: process.env.SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
